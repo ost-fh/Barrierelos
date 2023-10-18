389b1c27d17414d631af89fa52dbb9b4ccc5a61f
Pascal Lehmann <pascal.lehmann@ost.ch>
2023-10-18 16:23:37 +0200

    Install Material UI

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {CssBaseline} from "@mui/material";
 
 const container = document.getElementById('root')
 if (container === null)
@@ -9,6 +10,7 @@ if (container === null)
 
 ReactDOM.createRoot(container).render(
   <React.StrictMode>
+    <CssBaseline/>
     <App/>
   </React.StrictMode>,
 )

915d77ff1cbcdefc733e7a7aacb40d09748ed86f
Michael Hofmann <michael.hofmann@ost.ch>
2023-10-22 11:33:51 +0200

    Add gradle

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  kotlin("jvm") version "1.8.22"
+  kotlin("plugin.spring") version "1.8.22"
+  kotlin("plugin.jpa") version "1.8.22"
+  id("org.springframework.boot") version "3.1.5"
+}
+
+group = "ch.barrierelos"
+version = "0.0.0-SNAPSHOT"
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.10")
+  implementation("org.springframework.boot:spring-boot-starter-web:3.1.5")
+  implementation("org.yaml:snakeyaml:2.2")
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions {
+    freeCompilerArgs += "-Xjsr305=strict"
+    jvmTarget = "17"
+  }
+}

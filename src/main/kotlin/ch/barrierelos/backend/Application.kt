119b1aacb9fc15bff4965a2c825c5cf5a3fbe4ad
Michael Hofmann <michael.hofmann@ost.ch>
2023-10-22 11:42:11 +0200

    Add spring

---
+++ src/main/kotlin/ch/barrierelos/backend/Application.kt
@@ -0,0 +1,12 @@
+package ch.barrierelos.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>)
+{
+  runApplication<Application>(*args)
+}

e6cbbcd8757ea8d10b2d89773a2bbb3b5dabb0fb
Pascal Lehmann <pascal.lehmann@ost.ch>
2023-10-23 11:58:56 +0200

    Initialize project
    Also added a custom .gitignore.

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        node: true,
+        commonjs: true,
+        es2021: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+    },
+    rules: {}
+}

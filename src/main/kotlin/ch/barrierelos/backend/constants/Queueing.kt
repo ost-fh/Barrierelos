0d6a4dd598a22a5be2d193b20fee16e955abb4a8
Michael Hofmann <michael.hofmann@ost.ch>
2023-11-06 16:23:00 +0100

    Add queuing constants

---
+++ src/main/kotlin/ch/barrierelos/backend/constants/Queueing.kt
@@ -0,0 +1,9 @@
+package ch.barrierelos.backend.constants
+
+public object Queueing
+{
+  public const val EXCHANGE_ANALYSIS: String = ""
+
+  public const val QUEUE_JOB: String = "barrierelos.analysis.job"
+  public const val QUEUE_RESULT: String = "barrierelos.analysis.result"
+}

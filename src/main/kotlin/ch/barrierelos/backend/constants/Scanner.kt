8f497c16b112df5a6a061038de6492dc30dccec1
Michael Hofmann <michael.hofmann@ost.ch>
2023-11-09 03:41:51 +0100

    Add analysis converter

--- src/main/kotlin/ch/barrierelos/backend/constants/Scanner.kt
+++ src/main/kotlin/ch/barrierelos/backend/constants/Scanner.kt
@@ -2,5 +2,5 @@ package ch.barrierelos.backend.constants
 
 public object Scanner
 {
-  public const val MODEL_VERSION = "0.0.0"
+  public const val MODEL_VERSION: String = "0.0.0"
 }
---
+++ src/main/kotlin/ch/barrierelos/backend/converter/AnalysisConverter.kt
@@ -0,0 +1,185 @@
+package ch.barrierelos.backend.converter
+
+import ch.barrierelos.backend.constants.Scanner.MODEL_VERSION
+import ch.barrierelos.backend.entity.scanner.*
+import ch.barrierelos.backend.message.WebsiteMessage
+import ch.barrierelos.backend.message.scanner.*
+import ch.barrierelos.backend.model.scanner.*
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.toJavaInstant
+import java.sql.Timestamp
+import java.time.format.DateTimeFormatter
+
+public fun WebsiteMessage.toAnalysisJobMessage(): AnalysisJobMessage
+{
+  return AnalysisJobMessage(
+    modelVersion = MODEL_VERSION,
+    jobTimestamp = DateTimeFormatter.ISO_INSTANT.format(Clock.System.now().toJavaInstant()),
+    websiteBaseUrl = this.website,
+    webpagePaths = this.webpages,
+  )
+}
+
+public fun AnalysisJobMessage.toEntity(): AnalysisJobEntity
+{
+  return AnalysisJobEntity(
+    modelVersion = this.modelVersion,
+    websiteBaseUrl = this.websiteBaseUrl,
+    webpagePaths = this.webpagePaths,
+    modified = Timestamp.from(Instant.parse(this.jobTimestamp).toJavaInstant()),
+  )
+}
+
+public fun AnalysisJobEntity.toModel(): AnalysisJob
+{
+  return AnalysisJob(
+    id = this.analysisJobId,
+    modelVersion = this.modelVersion,
+    websiteBaseUrl = this.websiteBaseUrl,
+    webpagePaths = this.webpagePaths,
+    modified = this.modified.time,
+  )
+}
+
+public fun AnalysisResultMessage.toEntity(): AnalysisResultEntity
+{
+  return AnalysisResultEntity(
+    modelVersion = this.modelVersion,
+    website = this.website,
+    scanStatus = this.scanStatus,
+    errorMessage = this.errorMessage,
+    modified = Timestamp(System.currentTimeMillis()),
+  )
+}
+
+public fun WebpageResultMessage.toEntity(analysisResult: AnalysisResultEntity, timestamp: Timestamp): WebpageResultEntity
+{
+  return WebpageResultEntity(
+    analysisResult = analysisResult,
+    path = this.path,
+    scanStatus = this.scanStatus,
+    errorMessage = this.errorMessage,
+    modified = timestamp,
+  )
+}
+
+public fun RuleMessage.toEntity(webpageResult: WebpageResultEntity, timestamp: Timestamp): RuleEntity
+{
+  return RuleEntity(
+    webpageResult = webpageResult,
+    code = this.id,
+    modified = timestamp,
+  )
+}
+
+public fun CheckMessage.toEntity(rule: RuleEntity, timestamp: Timestamp): CheckEntity
+{
+  return CheckEntity(
+    rule = rule,
+    code = this.id,
+    type = this.type,
+    impact = this.impact,
+    testedCount = this.testedCount,
+    passedCount = this.passedCount,
+    violatedCount = this.violatedCount,
+    incompleteCount = this.incompleteCount,
+    modified = timestamp,
+  )
+}
+
+public fun CheckElementMessage.toEntity(timestamp: Timestamp): CheckElementEntity
+{
+  return CheckElementEntity(
+    target = this.target,
+    html = this.html,
+    issueDescription = this.issueDescription,
+    data = this.data,
+    modified = timestamp,
+  )
+}
+
+public fun ElementMessage.toEntity(checkElement: CheckElementEntity, timestamp: Timestamp): ElementEntity
+{
+  return ElementEntity(
+    checkElement = checkElement,
+    target = this.target,
+    html = this.html,
+    modified = timestamp,
+  )
+}
+
+public fun AnalysisResultEntity.toModel(): AnalysisResult
+{
+  return AnalysisResult(
+    id = this.analysisResultId,
+    modelVersion = this.modelVersion,
+    website = this.website,
+    scanStatus = this.scanStatus,
+    errorMessage = this.errorMessage,
+    webpages = this.webpages.map { it.toModel() }.toMutableSet(),
+    modified = this.modified.time,
+  )
+}
+
+public fun WebpageResultEntity.toModel(): WebpageResult
+{
+  return WebpageResult(
+    id = this.webpageResultId,
+    path = this.path,
+    scanStatus = this.scanStatus,
+    errorMessage = this.errorMessage,
+    rules = this.rules.map { it.toModel() }.toMutableSet(),
+    modified = this.modified.time,
+  )
+}
+
+public fun RuleEntity.toModel(): Rule
+{
+  return Rule(
+    id = this.ruleId,
+    code = this.code,
+    checks = this.checks.map { it.toModel() }.toMutableSet(),
+    modified = this.modified.time,
+  )
+}
+
+public fun CheckEntity.toModel(): Check
+{
+  return Check(
+    id = this.checkId,
+    code = this.code,
+    type = this.type,
+    impact = this.impact,
+    testedCount = this.testedCount,
+    passedCount = this.passedCount,
+    violatedCount = this.violatedCount,
+    incompleteCount = this.incompleteCount,
+    violatingElements = this.violatingElements.map { it.toModel() }.toMutableSet(),
+    incompleteElements = this.incompleteElements.map { it.toModel() }.toMutableSet(),
+    modified = this.modified.time,
+  )
+}
+
+public fun CheckElementEntity.toModel(): CheckElement
+{
+  return CheckElement(
+    id = this.checkElementId,
+    target = this.target,
+    html = this.html,
+    issueDescription = this.issueDescription,
+    data = this.data,
+    relatedElements = this.relatedElements.map { it.toModel() }.toMutableSet(),
+    modified = this.modified.time,
+  )
+}
+
+public fun ElementEntity.toModel(): Element
+{
+  return Element(
+    id = this.elementId,
+    target = this.target,
+    html = this.html,
+    modified = this.modified.time,
+  )
+}

32b471ae083ab106f3f262324eb3bb20e694e5b9
Michael Hofmann <michael.hofmann@ost.ch>
2023-11-30 23:39:23 +0100

    Update repository

--- src/main/kotlin/ch/barrierelos/backend/repository/Repository.kt
+++ src/main/kotlin/ch/barrierelos/backend/repository/Repository.kt
@@ -17,6 +17,14 @@ import java.sql.Timestamp
 @NoRepositoryBean
 public interface Repository<E> : JpaRepository<E, Long>
 {
+  public fun findAllByCreatedAfterAndModifiedAfter(created: Timestamp, modified: Timestamp, pageable: Pageable): Page<E>
+
+  public fun findAllByCreatedAfterAndModifiedAfter(created: Timestamp, modified: Timestamp, sort: Sort): List<E>
+
+  public fun findAllByCreatedAfter(created: Timestamp, pageable: Pageable): Page<E>
+
+  public fun findAllByCreatedAfter(created: Timestamp, sort: Sort): List<E>
+
   public fun findAllByModifiedAfter(modified: Timestamp, pageable: Pageable): Page<E>
 
   public fun findAllByModifiedAfter(modified: Timestamp, sort: Sort): List<E>
@@ -33,13 +41,21 @@ public interface Repository<E> : JpaRepository<E, Long>
       if(!this.existsById(id)) throw NoSuchElementException()
     }
 
+    private fun <E> Repository<E>.findAllByCreatedAfterAndModifiedAfter(created: Long, modified: Long, pageable: Pageable): Page<E> = findAllByCreatedAfterAndModifiedAfter(Timestamp(created), Timestamp(modified), pageable)
+
+    private fun <E> Repository<E>.findAllByCreatedAfterAndModifiedAfter(created: Long, modified: Long, sort: Sort): List<E> = findAllByCreatedAfterAndModifiedAfter(Timestamp(created), Timestamp(modified), sort)
+
+    private fun <E> Repository<E>.findAllByCreatedAfter(created: Long, pageable: Pageable): Page<E> = findAllByCreatedAfter(Timestamp(created), pageable)
+
+    private fun <E> Repository<E>.findAllByCreatedAfter(created: Long, sort: Sort): List<E> = findAllByCreatedAfter(Timestamp(created), sort)
+
     private fun <E> Repository<E>.findAllByModifiedAfter(modified: Long, pageable: Pageable): Page<E> = findAllByModifiedAfter(Timestamp(modified), pageable)
 
     private fun <E> Repository<E>.findAllByModifiedAfter(modified: Long, sort: Sort): List<E> = findAllByModifiedAfter(Timestamp(modified), sort)
 
-    public fun <E, M> Repository<E>.findAll(defaultParameters: DefaultParameters, entity: Class<E>, toModel: E.() -> M): Result<M> = findAll(defaultParameters.page, defaultParameters.size, defaultParameters.sort, defaultParameters.order, defaultParameters.modifiedAfter, entity, toModel)
+    public fun <E, M> Repository<E>.findAll(defaultParameters: DefaultParameters, entity: Class<E>, toModel: E.() -> M): Result<M> = findAll(defaultParameters.page, defaultParameters.size, defaultParameters.sort, defaultParameters.order, defaultParameters.createdAfter, defaultParameters.modifiedAfter, entity, toModel)
 
-    public fun <E, M> Repository<E>.findAll(page: Int?, size: Int?, sort: String?, order: OrderEnum?, modifiedAfter: Long?, entity: Class<E>, toModel: E.() -> M): Result<M>
+    public fun <E, M> Repository<E>.findAll(page: Int?, size: Int?, sort: String?, order: OrderEnum?, createdAfter: Long?, modifiedAfter: Long?, entity: Class<E>, toModel: E.() -> M): Result<M>
     {
       var sortStr = (if (sort == "id") null else sort)
         ?: (entity.declaredFields.find { it.isAnnotationPresent(Id::class.java) })?.name
@@ -73,24 +89,40 @@ public interface Repository<E> : JpaRepository<E, Long>
       {
         val pageable = PageRequest.of(page, size).withSort(sorting)
 
-        if(modifiedAfter == null)
+        if(createdAfter != null && modifiedAfter != null)
         {
-          this.findAll(pageable).toResult(count, lastModified, toModel)
+          this.findAllByCreatedAfterAndModifiedAfter(createdAfter, modifiedAfter, pageable).toResult(count, lastModified, toModel)
+        }
+        else if(createdAfter != null)
+        {
+          this.findAllByCreatedAfter(createdAfter, pageable).toResult(count, lastModified, toModel)
+        }
+        else if(modifiedAfter != null)
+        {
+          this.findAllByModifiedAfter(modifiedAfter, pageable).toResult(count, lastModified, toModel)
         }
         else
         {
-          this.findAllByModifiedAfter(modifiedAfter, pageable).toResult(count, lastModified, toModel)
+          this.findAll(pageable).toResult(count, lastModified, toModel)
         }
       }
       else
       {
-        if(modifiedAfter == null)
+        if(createdAfter != null && modifiedAfter != null)
         {
-          Result(this.findAll(sorting).map { it.toModel() }, count, lastModified)
+          Result(this.findAllByCreatedAfterAndModifiedAfter(createdAfter, modifiedAfter, sorting).map { it.toModel() }, count, lastModified)
+        }
+        else if(createdAfter != null)
+        {
+          Result(this.findAllByCreatedAfter(createdAfter, sorting).map { it.toModel() }, count, lastModified)
+        }
+        else if(modifiedAfter != null)
+        {
+          Result(this.findAllByModifiedAfter(modifiedAfter, sorting).map { it.toModel() }, count, lastModified)
         }
         else
         {
-          Result(this.findAllByModifiedAfter(modifiedAfter, sorting).map { it.toModel() }, count, lastModified)
+          Result(this.findAll(sorting).map { it.toModel() }, count, lastModified)
         }
       }
     }

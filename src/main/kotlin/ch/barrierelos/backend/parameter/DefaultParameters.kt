eb6a2eaa2f611ddd7e1ff41c4c6c4a367735ffb8
Michael Hofmann <michael.hofmann@ost.ch>
2023-11-30 23:38:48 +0100

    Add created to default parameter

--- src/main/kotlin/ch/barrierelos/backend/parameter/DefaultParameters.kt
+++ src/main/kotlin/ch/barrierelos/backend/parameter/DefaultParameters.kt
@@ -8,5 +8,6 @@ public data class DefaultParameters
   public val size: Int? = null,
   public val sort: String? = null,
   public val order: OrderEnum? = null,
-  public val modifiedAfter: Long? = null
+  public val modifiedAfter: Long? = null,
+  public val createdAfter: Long? = null,
 )

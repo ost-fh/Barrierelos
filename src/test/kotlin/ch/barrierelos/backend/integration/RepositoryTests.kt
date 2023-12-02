e506a176271f576c01fe89638847cf37e96b52dc
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-02 15:31:43 +0100

    Add tag repository tests

--- src/test/kotlin/ch/barrierelos/backend/integration/RepositoryTests.kt
+++ src/test/kotlin/ch/barrierelos/backend/integration/RepositoryTests.kt
@@ -1,9 +1,10 @@
 package ch.barrierelos.backend.integration
 
-import ch.barrierelos.backend.helper.createCredentialEntity
-import ch.barrierelos.backend.helper.createUserEntity
-import ch.barrierelos.backend.repository.CredentialRepository
-import ch.barrierelos.backend.repository.UserRepository
+import ch.barrierelos.backend.helper.*
+import ch.barrierelos.backend.repository.*
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.collections.shouldNotBeEmpty
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -21,6 +22,8 @@ class RepositoryTests
   lateinit var userRepository: UserRepository
   @Autowired
   lateinit var credentialRepository: CredentialRepository
+  @Autowired
+  lateinit var tagRepository: TagRepository
 
   @Nested
   inner class UserRepositoryTests
@@ -231,6 +234,7 @@ class RepositoryTests
       if(actual != null)
       {
         assertNotEquals(0, actual.credentialId)
+        assertEquals(expected.credentialId, actual.credentialId)
         assertEquals(expected.userFk, actual.userFk)
         assertEquals(expected.password, actual.password)
         assertEquals(expected.issuer, actual.issuer)
@@ -270,6 +274,7 @@ class RepositoryTests
       if(actual != null)
       {
         assertNotEquals(0, actual.credentialId)
+        assertEquals(expected.credentialId, actual.credentialId)
         assertEquals(expected.userFk, actual.userFk)
         assertEquals(expected.password, actual.password)
         assertEquals(expected.issuer, actual.issuer)
@@ -303,6 +308,71 @@ class RepositoryTests
 
       // then
       val actual = credentialRepository.findById(5000).orElse(null)
+
+      assertNull(actual)
+    }
+  }
+
+  @Nested
+  inner class TagRepositoryTests
+  {
+    @Test
+    fun `finds tags, when tags exist`()
+    {
+      // when
+      entityManager.persist(createTagEntity().apply { name = "one" })
+      entityManager.persist(createTagEntity().apply { name = "two" })
+      entityManager.persist(createTagEntity().apply { name = "three" })
+      entityManager.persist(createTagEntity().apply { name = "four" })
+      entityManager.flush()
+
+      // then
+      val actual = tagRepository.findAll()
+
+      assertNotNull(actual)
+      actual.shouldNotBeEmpty()
+      actual.shouldHaveSize(4)
+    }
+
+    @Test
+    fun `cannot find tags, when no tags exist`()
+    {
+      // then
+      val actual = tagRepository.findAll()
+
+      assertNotNull(actual)
+      actual.shouldBeEmpty()
+      actual.shouldHaveSize(0)
+    }
+
+    @Test
+    fun `find tag by name, when tag exists`()
+    {
+      // when
+      val expected = createTagEntity()
+
+      entityManager.persist(expected)
+      entityManager.flush()
+
+      // then
+      val actual = tagRepository.findByName(expected.name)
+
+      if(actual != null)
+      {
+        assertNotEquals(0, actual.tagId)
+        assertEquals(expected.tagId, actual.tagId)
+        assertEquals(expected.name, actual.name)
+      }
+    }
+
+    @Test
+    fun `cannot find tag by name, when tag not exists`()
+    {
+      // when
+      val expected = createTagEntity()
+
+      // then
+      val actual = tagRepository.findByName(expected.name)
 
       assertNull(actual)
     }

4a1844638225ad2d0b07f5cb2899bd1fcd95e9d0
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-02 15:52:51 +0100

    Add enums

---
+++ src/main/kotlin/ch/barrierelos/backend/enums/CategoryEnum.kt
@@ -0,0 +1,13 @@
+package ch.barrierelos.backend.enums
+
+public enum class CategoryEnum
+{
+  GOVERNMENT_FEDERAL,
+  GOVERNMENT_CANTONAL,
+  GOVERNMENT_MUNICIPAL,
+  PRIVATE_AFFILIATED,
+  PRIVATE_UNIVERSITY,
+  PRIVATE_NEWS,
+  PRIVATE_SHOP,
+  PRIVATE_OTHER
+}
---
+++ src/main/kotlin/ch/barrierelos/backend/enums/StatusEnum.kt
@@ -0,0 +1,9 @@
+package ch.barrierelos.backend.enums
+
+public enum class StatusEnum
+{
+  BLOCKED,
+  READY,
+  PENDING_INITIAL,
+  PENDING_RESCAN
+}

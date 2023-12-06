a409d1eba6c6434e30cf045ee6111787ec7e3441
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-06 19:45:55 +0100

    Add website tag repository

--- src/main/kotlin/ch/barrierelos/backend/repository/WebsiteTagRepository.kt
+++ src/main/kotlin/ch/barrierelos/backend/repository/WebsiteTagRepository.kt
@@ -1,5 +1,10 @@
 package ch.barrierelos.backend.repository
 
 import ch.barrierelos.backend.entity.WebsiteTagEntity
+import org.springframework.transaction.annotation.Transactional
 
 public interface WebsiteTagRepository : Repository<WebsiteTagEntity>
+{
+  @Transactional
+  public fun deleteAllByWebsiteFk(websiteFk: Long)
+}

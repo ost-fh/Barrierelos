663d4afc8c994ef3650fbb43a3a84f3f94394531
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-06 19:27:35 +0100

    Update user repository

--- src/main/kotlin/ch/barrierelos/backend/repository/UserRepository.kt
+++ src/main/kotlin/ch/barrierelos/backend/repository/UserRepository.kt
@@ -9,4 +9,6 @@ public interface UserRepository : Repository<UserEntity>
 
   @Query("SELECT u FROM UserEntity u, CredentialEntity c WHERE u.userId = c.userFk AND c.issuer = ?1 AND c.subject = ?2")
   public fun findByIssuerAndSubject(issuer: String, subject: String): UserEntity?
+
+  public fun existsByUsername(username: String): Boolean
 }

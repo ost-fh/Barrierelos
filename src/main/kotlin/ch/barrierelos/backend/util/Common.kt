ee03594826aa748018afb57aad375dc5780db0ff
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-06 19:23:26 +0100

    Update utils

--- src/main/kotlin/ch/barrierelos/backend/util/Common.kt
+++ src/main/kotlin/ch/barrierelos/backend/util/Common.kt
@@ -19,9 +19,21 @@ public fun <T> Optional<T>.orThrow(exception: Exception): T
   return this.orElseThrow { exception }
 }
 
+public fun <T> MutableCollection<T>.clearAndAdd(element: T): Boolean
+{
+  this.clear()
+
+  return this.add(element)
+}
+
 public fun <T> MutableCollection<T>.clearAndAddAll(elements: Collection<T>): Boolean
 {
   this.clear()
 
   return this.addAll(elements)
+}
+
+public fun <T, K> Collection<T>.containsDuplicates(selector: (T) -> K): Boolean
+{
+  return this.size != this.distinctBy(selector).size
 }

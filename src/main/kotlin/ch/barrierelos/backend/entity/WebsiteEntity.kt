06c7fbd62173ffc867ca546170d99fd60b936633
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-06 19:48:32 +0100

    Add website entity

--- src/main/kotlin/ch/barrierelos/backend/entity/WebsiteEntity.kt
+++ src/main/kotlin/ch/barrierelos/backend/entity/WebsiteEntity.kt
@@ -13,16 +13,17 @@ public class WebsiteEntity
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   public var websiteId: Long = 0,
   public var userFk: Long,
+  @Column(unique = true)
   public var domain: String,
+  @Column(unique = true)
   public var url: String,
   @Enumerated(EnumType.STRING)
   public var category: CategoryEnum,
   @Enumerated(EnumType.STRING)
   public var status: StatusEnum = StatusEnum.PENDING_INITIAL,
-  @OneToMany(orphanRemoval=true, cascade=[CascadeType.ALL], fetch=FetchType.EAGER)
-  @JoinColumn(name="websiteFk")
+  @OneToMany(orphanRemoval = true, cascade = [CascadeType.ALL], fetch = FetchType.EAGER)
+  @JoinColumn(name = "websiteFk", referencedColumnName = "websiteId")
   public var tags: MutableSet<WebsiteTagEntity>,
-  public var webpageCount: Int,
-  public var modified: Timestamp = Timestamp(0),
-  public var created: Timestamp = Timestamp(0),
+  public var modified: Timestamp = Timestamp(System.currentTimeMillis()),
+  public var created: Timestamp = Timestamp(System.currentTimeMillis()),
 )

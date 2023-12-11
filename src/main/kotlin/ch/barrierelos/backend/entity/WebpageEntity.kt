cc45acfc7c6e4b688de714f8cb5dcf9999beedaa
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-11 17:03:31 +0100

    Add webpage entity

--- src/main/kotlin/ch/barrierelos/backend/entity/WebpageEntity.kt
+++ src/main/kotlin/ch/barrierelos/backend/entity/WebpageEntity.kt
@@ -1,6 +1,5 @@
 package ch.barrierelos.backend.entity
 
-import ch.barrierelos.backend.enums.CategoryEnum
 import ch.barrierelos.backend.enums.StatusEnum
 import jakarta.persistence.*
 import java.sql.Timestamp
@@ -8,18 +7,17 @@ import java.sql.Timestamp
 @Entity
 @Table(name = "webpage")
 public class WebpageEntity
-  (
+(
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   public var webpageId: Long = 0,
   public var websiteFk: Long,
   public var userFk: Long,
-  public var domain: String,
+  public var path: String,
+  @Column(unique = true)
   public var url: String,
   @Enumerated(EnumType.STRING)
-  public var category: CategoryEnum,
-  @Enumerated(EnumType.STRING)
-  public var status: StatusEnum,
-  public var modified: Timestamp = Timestamp(0),
-  public var created: Timestamp = Timestamp(0),
+  public var status: StatusEnum = StatusEnum.PENDING_INITIAL,
+  public var modified: Timestamp = Timestamp(System.currentTimeMillis()),
+  public var created: Timestamp = Timestamp(System.currentTimeMillis()),
 )

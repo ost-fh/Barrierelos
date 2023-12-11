8ff23d1ba17154476bc2d9f58573882c0ce9f621
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-11 17:04:32 +0100

    Add webpage repository

--- src/main/kotlin/ch/barrierelos/backend/repository/WebpageRepository.kt
+++ src/main/kotlin/ch/barrierelos/backend/repository/WebpageRepository.kt
@@ -3,3 +3,6 @@ package ch.barrierelos.backend.repository
 import ch.barrierelos.backend.entity.WebpageEntity
 
 public interface WebpageRepository : Repository<WebpageEntity>
+{
+  public fun existsByPathAndWebsiteFk(path: String, websiteFk: Long): Boolean
+}

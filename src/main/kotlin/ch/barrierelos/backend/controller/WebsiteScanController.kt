0cb1301866d6e4e21633517fab0f61efdf4ae030
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-18 08:28:04 +0100

    Add website scan controller

---
+++ src/main/kotlin/ch/barrierelos/backend/controller/WebsiteScanController.kt
@@ -0,0 +1,76 @@
+package ch.barrierelos.backend.controller
+
+import ch.barrierelos.backend.constants.Endpoint.WEBSITE_SCAN
+import ch.barrierelos.backend.constants.MediaType
+import ch.barrierelos.backend.model.WebsiteScan
+import ch.barrierelos.backend.model.WebsiteStatistic
+import ch.barrierelos.backend.parameter.DefaultParameters
+import ch.barrierelos.backend.service.WebsiteScanService
+import ch.barrierelos.backend.util.toHeaders
+import org.springdoc.core.annotations.ParameterObject
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+public class WebsiteScanController
+{
+  @Autowired
+  private lateinit var websiteScanService: WebsiteScanService
+
+  @Autowired
+  private lateinit var websiteStatisticController: WebsiteStatisticController
+
+  @PostMapping(value = [WEBSITE_SCAN], consumes = [MediaType.JSON], produces = [MediaType.JSON])
+  public fun addWebsiteScan(@RequestBody websiteScan: WebsiteScan): ResponseEntity<WebsiteScan>
+  {
+    websiteScan.id = 0
+
+    this.websiteScanService.addWebsiteScan(websiteScan)
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(websiteScan)
+  }
+
+  @PutMapping(value = ["$WEBSITE_SCAN/{id}"], consumes = [MediaType.JSON], produces = [MediaType.JSON])
+  public fun updateWebsiteScan(@PathVariable id: Long, @RequestBody websiteScan: WebsiteScan): ResponseEntity<WebsiteScan>
+  {
+    websiteScan.id = id
+
+    this.websiteScanService.updateWebsiteScan(websiteScan)
+
+    return ResponseEntity.status(HttpStatus.OK).body(websiteScan)
+  }
+
+  @GetMapping(value = [WEBSITE_SCAN], produces = [MediaType.JSON])
+  public fun getWebsiteScans(@ParameterObject defaultParameters: DefaultParameters): ResponseEntity<List<WebsiteScan>>
+  {
+    val websiteScansPage = this.websiteScanService.getWebsiteScans(defaultParameters)
+
+    return ResponseEntity.status(HttpStatus.OK).headers(websiteScansPage.toHeaders()).body(websiteScansPage.content)
+  }
+
+  @GetMapping(value = ["$WEBSITE_SCAN/{id}"], produces = [MediaType.JSON])
+  public fun getWebsiteScan(@PathVariable id: Long): ResponseEntity<WebsiteScan>
+  {
+    val websiteScan: WebsiteScan = this.websiteScanService.getWebsiteScan(id)
+
+    return ResponseEntity.status(HttpStatus.OK).body(websiteScan)
+  }
+
+  @GetMapping(value = ["$WEBSITE_SCAN/{id}/statistic"], produces = [MediaType.JSON])
+  public fun getWebsiteScanStatistic(@PathVariable id: Long): ResponseEntity<WebsiteStatistic>
+  {
+    val websiteScan: WebsiteScan = this.websiteScanService.getWebsiteScan(id)
+
+    return this.websiteStatisticController.getWebsiteStatistic(websiteScan.websiteStatisticId)
+  }
+
+  @DeleteMapping(value = ["$WEBSITE_SCAN/{id}"], produces = [MediaType.JSON])
+  public fun deleteWebsiteScan(@PathVariable id: Long): ResponseEntity<Void>
+  {
+    this.websiteScanService.deleteWebsiteScan(id)
+
+    return ResponseEntity.status(HttpStatus.OK).build()
+  }
+}

ddf805b95b4a92cfe968319a9ea6025561bf1256
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-19 16:03:46 +0100

    Add report repository

--- src/main/kotlin/ch/barrierelos/backend/repository/ReportRepository.kt
+++ src/main/kotlin/ch/barrierelos/backend/repository/ReportRepository.kt
@@ -3,3 +3,6 @@ package ch.barrierelos.backend.repository
 import ch.barrierelos.backend.entity.ReportEntity
 
 public interface ReportRepository : Repository<ReportEntity>
+{
+  public fun findAllByUserFk(userFk: Long): Set<ReportEntity>
+}

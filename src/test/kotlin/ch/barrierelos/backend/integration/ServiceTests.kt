c9c8959a12e44faf756f885579d865ccee1af688
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-20 10:59:43 +0100

    Add webpage report service tests

--- src/test/kotlin/ch/barrierelos/backend/integration/ServiceTests.kt
+++ src/test/kotlin/ch/barrierelos/backend/integration/ServiceTests.kt
@@ -63,6 +63,8 @@ class ServiceTests
   lateinit var userReportService: UserReportService
   @Autowired
   lateinit var websiteReportService: WebsiteReportService
+  @Autowired
+  lateinit var webpageReportService: WebpageReportService
 
   @Autowired
   lateinit var userRepository: UserRepository
@@ -97,6 +99,8 @@ class ServiceTests
   lateinit var userReportRepository: UserReportRepository
   @Autowired
   lateinit var websiteReportRepository: WebsiteReportRepository
+  @Autowired
+  lateinit var webpageReportRepository: WebpageReportRepository
 
 
   lateinit var admin: UserEntity
@@ -173,6 +177,7 @@ class ServiceTests
     webpageScanRepository.deleteAll()
     userReportRepository.deleteAll()
     websiteReportRepository.deleteAll()
+    webpageReportRepository.deleteAll()
     reportRepository.deleteAll()
     reportMessageRepository.deleteAll()
   }
@@ -5594,6 +5599,448 @@ class ServiceTests
         // then
         assertThrows(NoAuthorizationException::class.java) {
           websiteReportService.deleteWebsiteReport(websiteReport.id)
+        }
+      }
+    }
+  }
+
+  @Nested
+  inner class WebpageReportServiceTests
+  {
+    @Nested
+    inner class AddWebpageReportTests
+    {
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `adds webpage report, given admin account`()
+      {
+        // when
+        val expected = createWebpageReportModel()
+
+        // then
+        val actual = webpageReportService.addWebpageReport(expected.copy())
+
+        assertNotEquals(0, actual.id)
+        assertEquals(expected.webpageId, actual.webpageId)
+        assertNotEquals(0, actual.report.id)
+        assertEquals(expected.report.userId, actual.report.userId)
+        assertEquals(expected.report.reason, actual.report.reason)
+        assertEquals(expected.report.state, actual.report.state)
+        assertEquals(expected.report.modified, actual.report.modified)
+        assertEquals(expected.report.created, actual.report.created)
+      }
+
+      @Test
+      @WithUserDetails("moderator", setupBefore=TEST_EXECUTION)
+      fun `adds webpage report, given moderator account`()
+      {
+        // when
+        val expected = createWebpageReportModel()
+
+        // then
+        val actual = webpageReportService.addWebpageReport(expected.copy())
+
+        assertNotEquals(0, actual.id)
+        assertEquals(expected.webpageId, actual.webpageId)
+        assertNotEquals(0, actual.report.id)
+        assertEquals(expected.report.userId, actual.report.userId)
+        assertEquals(expected.report.reason, actual.report.reason)
+        assertEquals(expected.report.state, actual.report.state)
+        assertEquals(expected.report.modified, actual.report.modified)
+        assertEquals(expected.report.created, actual.report.created)
+      }
+
+      @Test
+      @WithUserDetails("contributor", setupBefore=TEST_EXECUTION)
+      fun `adds webpage report, given contributor account`()
+      {
+        // when
+        val expected = createWebpageReportModel()
+
+        // then
+        val actual = webpageReportService.addWebpageReport(expected.copy())
+
+        assertNotEquals(0, actual.id)
+        assertEquals(expected.webpageId, actual.webpageId)
+        assertNotEquals(0, actual.report.id)
+        assertEquals(expected.report.userId, actual.report.userId)
+        assertEquals(expected.report.reason, actual.report.reason)
+        assertEquals(expected.report.state, actual.report.state)
+        assertEquals(expected.report.modified, actual.report.modified)
+        assertEquals(expected.report.created, actual.report.created)
+      }
+
+      @Test
+      @WithUserDetails("viewer", setupBefore=TEST_EXECUTION)
+      fun `cannot add webpage report, given viewer account`()
+      {
+        // when
+        val webpageReport = createWebpageReportModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          webpageReportService.addWebpageReport(webpageReport)
+        }
+      }
+
+      @Test
+      fun `cannot add webpage report, given no account`()
+      {
+        // when
+        val webpageReport = createWebpageReportModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          webpageReportService.addWebpageReport(webpageReport)
+        }
+      }
+    }
+
+    @Nested
+    inner class GetWebpageReportsTests
+    {
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `gets webpage reports, given admin account`()
+      {
+        // given
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 40L))
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 60L))
+
+        // when
+        val webpageReports = webpageReportService.getWebpageReports().content
+
+        // then
+        webpageReports.shouldNotBeEmpty()
+        webpageReports.shouldHaveSize(2)
+        assertTrue(webpageReports.any { it.webpageId == 40L })
+        assertTrue(webpageReports.any { it.webpageId == 60L })
+      }
+
+      @Test
+      @WithUserDetails("moderator", setupBefore=TEST_EXECUTION)
+      fun `gets webpage reports, given moderator account`()
+      {
+        // given
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 40L))
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 60L))
+
+        // when
+        val webpageReports = webpageReportService.getWebpageReports().content
+
+        // then
+        webpageReports.shouldNotBeEmpty()
+        webpageReports.shouldHaveSize(2)
+        assertTrue(webpageReports.any { it.webpageId == 40L })
+        assertTrue(webpageReports.any { it.webpageId == 60L })
+      }
+
+      @Test
+      @WithUserDetails("contributor", setupBefore=TEST_EXECUTION)
+      fun `gets webpage reports, given contributor account`()
+      {
+        // given
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 40L))
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 60L))
+
+        // when
+        val webpageReports = webpageReportService.getWebpageReports().content
+
+        // then
+        webpageReports.shouldNotBeEmpty()
+        webpageReports.shouldHaveSize(2)
+        assertTrue(webpageReports.any { it.webpageId == 40L })
+        assertTrue(webpageReports.any { it.webpageId == 60L })
+      }
+
+      @Test
+      @WithUserDetails("viewer", setupBefore=TEST_EXECUTION)
+      fun `cannot get webpage reports, given viewer account`()
+      {
+        // when
+        webpageReportRepository.save(createWebpageReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          webpageReportService.getWebpageReports()
+        }
+      }
+
+      @Test
+      fun `cannot get webpage reports, given no account`()
+      {
+        // when
+        webpageReportRepository.save(createWebpageReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          webpageReportService.getWebpageReports()
+        }
+      }
+
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `gets webpage reports by webpageId, given admin account`()
+      {
+        // given
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 40L))
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 40L))
+
+        // when
+        val webpageReports = webpageReportService.getWebpageReportsByWebpage(40).content
+
+        // then
+        webpageReports.shouldNotBeEmpty()
+        webpageReports.shouldHaveSize(2)
+        assertTrue(webpageReports.any { it.webpageId == 40L })
+      }
+
+      @Test
+      @WithUserDetails("moderator", setupBefore=TEST_EXECUTION)
+      fun `gets webpage reports by webpageId, given moderator account`()
+      {
+        // given
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 40L))
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 40L))
+
+        // when
+        val webpageReports = webpageReportService.getWebpageReportsByWebpage(40).content
+
+        // then
+        webpageReports.shouldNotBeEmpty()
+        webpageReports.shouldHaveSize(2)
+        assertTrue(webpageReports.any { it.webpageId == 40L })
+      }
+
+      @Test
+      @WithUserDetails("contributor", setupBefore=TEST_EXECUTION)
+      fun `gets webpage reports by webpageId, given contributor account`()
+      {
+        // given
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 40L))
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 40L))
+
+        // when
+        val webpageReports = webpageReportService.getWebpageReportsByWebpage(40).content
+
+        // then
+        webpageReports.shouldNotBeEmpty()
+        webpageReports.shouldHaveSize(2)
+        assertTrue(webpageReports.any { it.webpageId == 40L })
+      }
+
+      @Test
+      @WithUserDetails("viewer", setupBefore=TEST_EXECUTION)
+      fun `cannot get webpage reports by webpageId, given viewer account`()
+      {
+        // when
+        webpageReportRepository.save(createWebpageReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          webpageReportService.getWebpageReportsByWebpage(1)
+        }
+      }
+
+      @Test
+      fun `cannot get webpage reports by webpageId, given no account`()
+      {
+        // when
+        webpageReportRepository.save(createWebpageReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          webpageReportService.getWebpageReportsByWebpage(1)
+        }
+      }
+
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `gets webpage reports with headers, when no parameters, given admin account`()
+      {
+        // given
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 40L))
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 60L))
+
+        // when
+        val webpageReports = webpageReportService.getWebpageReports()
+
+        // then
+        webpageReports.page.shouldBe(0)
+        webpageReports.size.shouldBe(2)
+        webpageReports.totalElements.shouldBe(2)
+        webpageReports.totalPages.shouldBe(1)
+        webpageReports.count.shouldBe(2)
+        webpageReports.content.shouldHaveSize(2)
+      }
+
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `gets webpage reports with headers, when with parameters, given admin account`()
+      {
+        // given
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 40L))
+        webpageReportRepository.save(createWebpageReportEntity(webpageFk = 60L))
+
+        // when
+        val webpageReports = webpageReportService.getWebpageReports(DefaultParameters(
+          page = 1,
+          size = 1,
+          sort = "id",
+          order = OrderEnum.ASC
+        ))
+
+        // then
+        webpageReports.page.shouldBe(1)
+        webpageReports.size.shouldBe(1)
+        webpageReports.totalElements.shouldBe(2)
+        webpageReports.totalPages.shouldBe(2)
+        webpageReports.count.shouldBe(2)
+        webpageReports.content.shouldHaveSize(1)
+      }
+    }
+
+    @Nested
+    inner class GetWebpageReportTests
+    {
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `gets webpage report, given admin account`()
+      {
+        // when
+        val expected = webpageReportRepository.save(createWebpageReportEntity()).toModel()
+
+        // then
+        val actual = webpageReportService.getWebpageReport(expected.id)
+
+        assertEquals(expected, actual)
+      }
+
+      @Test
+      @WithUserDetails("moderator", setupBefore=TEST_EXECUTION)
+      fun `gets webpage report, given moderator account`()
+      {
+        // when
+        val expected = webpageReportRepository.save(createWebpageReportEntity()).toModel()
+
+        // then
+        val actual = webpageReportService.getWebpageReport(expected.id)
+
+        assertEquals(expected, actual)
+      }
+
+      @Test
+      @WithUserDetails("contributor", setupBefore=TEST_EXECUTION)
+      fun `gets webpage report, given contributor account`()
+      {
+        // when
+        val expected = webpageReportRepository.save(createWebpageReportEntity()).toModel()
+
+        // then
+        val actual = webpageReportService.getWebpageReport(expected.id)
+
+        assertEquals(expected, actual)
+      }
+
+      @Test
+      @WithUserDetails("viewer", setupBefore=TEST_EXECUTION)
+      fun `cannot get webpage report, given viewer account`()
+      {
+        // when
+        val webpageReport = webpageReportRepository.save(createWebpageReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          webpageReportService.getWebpageReport(webpageReport.id)
+        }
+      }
+
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `cannot get webpageReport, when webpageReport not exists`()
+      {
+        assertThrows(NoSuchElementException::class.java) {
+          webpageReportService.getWebpageReport(5000000)
+        }
+      }
+    }
+
+    @Nested
+    inner class DeleteWebpageReportTests
+    {
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `deletes webpage report, given admin account`()
+      {
+        // when
+        val webpageReport = webpageReportRepository.save(createWebpageReportEntity()).toModel()
+        val webpageReportsBefore = webpageReportService.getWebpageReports().content
+
+        // then
+        assertDoesNotThrow {
+          webpageReportService.deleteWebpageReport(webpageReport.id)
+        }
+
+        assertThrows(NoSuchElementException::class.java) {
+          webpageReportService.getWebpageReport(webpageReport.id)
+        }
+
+        val webpageReportsAfter = webpageReportService.getWebpageReports().content
+
+        webpageReportsBefore.shouldContain(webpageReport)
+        webpageReportsAfter.shouldNotContain(webpageReport)
+        webpageReportsBefore.shouldContainAll(webpageReportsAfter)
+        webpageReportsAfter.shouldNotContainAll(webpageReportsBefore)
+      }
+
+      @Test
+      @WithUserDetails("moderator", setupBefore=TEST_EXECUTION)
+      fun `cannot delete webpage report, given moderator account`()
+      {
+        // when
+        val webpageReport = webpageReportRepository.save(createWebpageReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          webpageReportService.deleteWebpageReport(webpageReport.id)
+        }
+      }
+
+      @Test
+      @WithUserDetails("contributor", setupBefore=TEST_EXECUTION)
+      fun `cannot delete webpage report, given contributor account`()
+      {
+        // when
+        val webpageReport = webpageReportRepository.save(createWebpageReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          webpageReportService.deleteWebpageReport(webpageReport.id)
+        }
+      }
+
+      @Test
+      @WithUserDetails("viewer", setupBefore=TEST_EXECUTION)
+      fun `cannot delete webpage report, given viewer account`()
+      {
+        // when
+        val webpageReport = webpageReportRepository.save(createWebpageReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          webpageReportService.deleteWebpageReport(webpageReport.id)
+        }
+      }
+
+      @Test
+      fun `cannot delete webpage report, given no account`()
+      {
+        // when
+        val webpageReport = webpageReportRepository.save(createWebpageReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          webpageReportService.deleteWebpageReport(webpageReport.id)
         }
       }
     }

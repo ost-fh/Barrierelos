6872226b72066684770a270dde6349a0d3649e31
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-20 05:54:09 +0100

    Add report message entity

--- src/main/kotlin/ch/barrierelos/backend/entity/ReportMessageEntity.kt
+++ src/main/kotlin/ch/barrierelos/backend/entity/ReportMessageEntity.kt
@@ -10,8 +10,8 @@ public class ReportMessageEntity
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   public var reportMessageId: Long = 0,
-  public var userFk: Long,
   public var reportFk: Long,
+  public var userFk: Long,
   public var message: String,
   public var modified: Timestamp = Timestamp(System.currentTimeMillis()),
   public var created: Timestamp = Timestamp(System.currentTimeMillis()),

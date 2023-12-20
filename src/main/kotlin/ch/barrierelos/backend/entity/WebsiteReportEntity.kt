712633a136eab8bafc1b7c73ad9ee74eb204035e
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-20 10:41:17 +0100

    Add website report entity

--- src/main/kotlin/ch/barrierelos/backend/entity/WebsiteReportEntity.kt
+++ src/main/kotlin/ch/barrierelos/backend/entity/WebsiteReportEntity.kt
@@ -1,17 +1,16 @@
 package ch.barrierelos.backend.entity
 
 import jakarta.persistence.*
-import java.sql.Timestamp
 
 @Entity
 @Table(name = "website_report")
 public class WebsiteReportEntity
-(
+  (
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   public var websiteReportId: Long = 0,
   public var websiteFk: Long,
-  public var reportFk: Long,
-  public var modified: Timestamp = Timestamp(System.currentTimeMillis()),
-  public var created: Timestamp = Timestamp(System.currentTimeMillis()),
+  @OneToOne(fetch = FetchType.EAGER, cascade = [CascadeType.ALL])
+  @JoinColumn(name = "report_fk", referencedColumnName = "reportId")
+  public var report: ReportEntity,
 )

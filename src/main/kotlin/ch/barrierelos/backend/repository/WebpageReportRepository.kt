aff1794bd66b1b86dbd1a23b444970113c6242a8
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-20 10:56:34 +0100

    Add webpage report repository

--- src/main/kotlin/ch/barrierelos/backend/repository/WebpageReportRepository.kt
+++ src/main/kotlin/ch/barrierelos/backend/repository/WebpageReportRepository.kt
@@ -1,5 +1,34 @@
 package ch.barrierelos.backend.repository
 
+import ch.barrierelos.backend.converter.toModel
 import ch.barrierelos.backend.entity.WebpageReportEntity
+import ch.barrierelos.backend.model.WebpageReport
+import ch.barrierelos.backend.parameter.DefaultParameters
+import ch.barrierelos.backend.repository.Repository.Companion.toPageable
+import ch.barrierelos.backend.util.Result
+import ch.barrierelos.backend.util.toResult
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
 
-public interface WebpageReportRepository : Repository<WebpageReportEntity>
+public interface WebpageReportRepository : JpaRepository<WebpageReportEntity, Long>
+{
+  public fun findAllByWebpageFk(webpageFk: Long, pageable: Pageable): Page<WebpageReportEntity>
+
+  public companion object
+  {
+    public fun WebpageReportRepository.findAll(defaultParameters: DefaultParameters = DefaultParameters()): Result<WebpageReport>
+    {
+      val pageable = defaultParameters.toPageable(WebpageReportEntity::class.java)
+
+      return this.findAll(pageable).toResult(WebpageReportEntity::toModel)
+    }
+
+    public fun WebpageReportRepository.findAllByWebpageFk(webpageFk: Long, defaultParameters: DefaultParameters = DefaultParameters()): Result<WebpageReport>
+    {
+      val pageable = defaultParameters.toPageable(WebpageReportEntity::class.java)
+
+      return this.findAllByWebpageFk(webpageFk, pageable).toResult(WebpageReportEntity::toModel)
+    }
+  }
+}

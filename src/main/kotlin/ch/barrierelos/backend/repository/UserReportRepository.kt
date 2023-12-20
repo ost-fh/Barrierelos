059c6af823c9a7d5b354e3d9d1ad31836b1b63ac
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-20 10:18:50 +0100

    Add user report repository

--- src/main/kotlin/ch/barrierelos/backend/repository/UserReportRepository.kt
+++ src/main/kotlin/ch/barrierelos/backend/repository/UserReportRepository.kt
@@ -1,5 +1,34 @@
 package ch.barrierelos.backend.repository
 
+import ch.barrierelos.backend.converter.toModel
 import ch.barrierelos.backend.entity.UserReportEntity
+import ch.barrierelos.backend.model.UserReport
+import ch.barrierelos.backend.parameter.DefaultParameters
+import ch.barrierelos.backend.repository.Repository.Companion.toPageable
+import ch.barrierelos.backend.util.Result
+import ch.barrierelos.backend.util.toResult
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
 
-public interface UserReportRepository : Repository<UserReportEntity>
+public interface UserReportRepository : JpaRepository<UserReportEntity, Long>
+{
+  public fun findAllByUserFk(userFk: Long, pageable: Pageable): Page<UserReportEntity>
+
+  public companion object
+  {
+    public fun UserReportRepository.findAll(defaultParameters: DefaultParameters = DefaultParameters()): Result<UserReport>
+    {
+      val pageable = defaultParameters.toPageable(UserReportEntity::class.java)
+
+      return this.findAll(pageable).toResult(UserReportEntity::toModel)
+    }
+
+    public fun UserReportRepository.findAllByUserFk(userFk: Long, defaultParameters: DefaultParameters = DefaultParameters()): Result<UserReport>
+    {
+      val pageable = defaultParameters.toPageable(UserReportEntity::class.java)
+
+      return this.findAllByUserFk(userFk, pageable).toResult(UserReportEntity::toModel)
+    }
+  }
+}

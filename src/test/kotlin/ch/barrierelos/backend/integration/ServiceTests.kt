43a938871e8b941576733cc9d671e70803c1252a
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-20 10:44:43 +0100

    Add website report service tests

--- src/test/kotlin/ch/barrierelos/backend/integration/ServiceTests.kt
+++ src/test/kotlin/ch/barrierelos/backend/integration/ServiceTests.kt
@@ -61,6 +61,8 @@ class ServiceTests
   lateinit var reportMessageService: ReportMessageService
   @Autowired
   lateinit var userReportService: UserReportService
+  @Autowired
+  lateinit var websiteReportService: WebsiteReportService
 
   @Autowired
   lateinit var userRepository: UserRepository
@@ -93,6 +95,8 @@ class ServiceTests
   lateinit var reportMessageRepository: ReportMessageRepository
   @Autowired
   lateinit var userReportRepository: UserReportRepository
+  @Autowired
+  lateinit var websiteReportRepository: WebsiteReportRepository
 
 
   lateinit var admin: UserEntity
@@ -168,6 +172,7 @@ class ServiceTests
     websiteScanRepository.deleteAll()
     webpageScanRepository.deleteAll()
     userReportRepository.deleteAll()
+    websiteReportRepository.deleteAll()
     reportRepository.deleteAll()
     reportMessageRepository.deleteAll()
   }
@@ -4801,7 +4806,7 @@ class ServiceTests
         }
       }
     }
-    
+
     @Nested
     inner class GetUserReportsTests
     {
@@ -5147,6 +5152,448 @@ class ServiceTests
         // then
         assertThrows(NoAuthorizationException::class.java) {
           userReportService.deleteUserReport(userReport.id)
+        }
+      }
+    }
+  }
+
+  @Nested
+  inner class WebsiteReportServiceTests
+  {
+    @Nested
+    inner class AddWebsiteReportTests
+    {
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `adds website report, given admin account`()
+      {
+        // when
+        val expected = createWebsiteReportModel()
+
+        // then
+        val actual = websiteReportService.addWebsiteReport(expected.copy())
+
+        assertNotEquals(0, actual.id)
+        assertEquals(expected.websiteId, actual.websiteId)
+        assertNotEquals(0, actual.report.id)
+        assertEquals(expected.report.userId, actual.report.userId)
+        assertEquals(expected.report.reason, actual.report.reason)
+        assertEquals(expected.report.state, actual.report.state)
+        assertEquals(expected.report.modified, actual.report.modified)
+        assertEquals(expected.report.created, actual.report.created)
+      }
+
+      @Test
+      @WithUserDetails("moderator", setupBefore=TEST_EXECUTION)
+      fun `adds website report, given moderator account`()
+      {
+        // when
+        val expected = createWebsiteReportModel()
+
+        // then
+        val actual = websiteReportService.addWebsiteReport(expected.copy())
+
+        assertNotEquals(0, actual.id)
+        assertEquals(expected.websiteId, actual.websiteId)
+        assertNotEquals(0, actual.report.id)
+        assertEquals(expected.report.userId, actual.report.userId)
+        assertEquals(expected.report.reason, actual.report.reason)
+        assertEquals(expected.report.state, actual.report.state)
+        assertEquals(expected.report.modified, actual.report.modified)
+        assertEquals(expected.report.created, actual.report.created)
+      }
+
+      @Test
+      @WithUserDetails("contributor", setupBefore=TEST_EXECUTION)
+      fun `adds website report, given contributor account`()
+      {
+        // when
+        val expected = createWebsiteReportModel()
+
+        // then
+        val actual = websiteReportService.addWebsiteReport(expected.copy())
+
+        assertNotEquals(0, actual.id)
+        assertEquals(expected.websiteId, actual.websiteId)
+        assertNotEquals(0, actual.report.id)
+        assertEquals(expected.report.userId, actual.report.userId)
+        assertEquals(expected.report.reason, actual.report.reason)
+        assertEquals(expected.report.state, actual.report.state)
+        assertEquals(expected.report.modified, actual.report.modified)
+        assertEquals(expected.report.created, actual.report.created)
+      }
+
+      @Test
+      @WithUserDetails("viewer", setupBefore=TEST_EXECUTION)
+      fun `cannot add website report, given viewer account`()
+      {
+        // when
+        val websiteReport = createWebsiteReportModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          websiteReportService.addWebsiteReport(websiteReport)
+        }
+      }
+
+      @Test
+      fun `cannot add website report, given no account`()
+      {
+        // when
+        val websiteReport = createWebsiteReportModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          websiteReportService.addWebsiteReport(websiteReport)
+        }
+      }
+    }
+
+    @Nested
+    inner class GetWebsiteReportsTests
+    {
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `gets website reports, given admin account`()
+      {
+        // given
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 40L))
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 60L))
+
+        // when
+        val websiteReports = websiteReportService.getWebsiteReports().content
+
+        // then
+        websiteReports.shouldNotBeEmpty()
+        websiteReports.shouldHaveSize(2)
+        assertTrue(websiteReports.any { it.websiteId == 40L })
+        assertTrue(websiteReports.any { it.websiteId == 60L })
+      }
+
+      @Test
+      @WithUserDetails("moderator", setupBefore=TEST_EXECUTION)
+      fun `gets website reports, given moderator account`()
+      {
+        // given
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 40L))
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 60L))
+
+        // when
+        val websiteReports = websiteReportService.getWebsiteReports().content
+
+        // then
+        websiteReports.shouldNotBeEmpty()
+        websiteReports.shouldHaveSize(2)
+        assertTrue(websiteReports.any { it.websiteId == 40L })
+        assertTrue(websiteReports.any { it.websiteId == 60L })
+      }
+
+      @Test
+      @WithUserDetails("contributor", setupBefore=TEST_EXECUTION)
+      fun `gets website reports, given contributor account`()
+      {
+        // given
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 40L))
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 60L))
+
+        // when
+        val websiteReports = websiteReportService.getWebsiteReports().content
+
+        // then
+        websiteReports.shouldNotBeEmpty()
+        websiteReports.shouldHaveSize(2)
+        assertTrue(websiteReports.any { it.websiteId == 40L })
+        assertTrue(websiteReports.any { it.websiteId == 60L })
+      }
+
+      @Test
+      @WithUserDetails("viewer", setupBefore=TEST_EXECUTION)
+      fun `cannot get website reports, given viewer account`()
+      {
+        // when
+        websiteReportRepository.save(createWebsiteReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          websiteReportService.getWebsiteReports()
+        }
+      }
+
+      @Test
+      fun `cannot get website reports, given no account`()
+      {
+        // when
+        websiteReportRepository.save(createWebsiteReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          websiteReportService.getWebsiteReports()
+        }
+      }
+
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `gets website reports by websiteId, given admin account`()
+      {
+        // given
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 40L))
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 40L))
+
+        // when
+        val websiteReports = websiteReportService.getWebsiteReportsByWebsite(40).content
+
+        // then
+        websiteReports.shouldNotBeEmpty()
+        websiteReports.shouldHaveSize(2)
+        assertTrue(websiteReports.any { it.websiteId == 40L })
+      }
+
+      @Test
+      @WithUserDetails("moderator", setupBefore=TEST_EXECUTION)
+      fun `gets website reports by websiteId, given moderator account`()
+      {
+        // given
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 40L))
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 40L))
+
+        // when
+        val websiteReports = websiteReportService.getWebsiteReportsByWebsite(40).content
+
+        // then
+        websiteReports.shouldNotBeEmpty()
+        websiteReports.shouldHaveSize(2)
+        assertTrue(websiteReports.any { it.websiteId == 40L })
+      }
+
+      @Test
+      @WithUserDetails("contributor", setupBefore=TEST_EXECUTION)
+      fun `gets website reports by websiteId, given contributor account`()
+      {
+        // given
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 40L))
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 40L))
+
+        // when
+        val websiteReports = websiteReportService.getWebsiteReportsByWebsite(40).content
+
+        // then
+        websiteReports.shouldNotBeEmpty()
+        websiteReports.shouldHaveSize(2)
+        assertTrue(websiteReports.any { it.websiteId == 40L })
+      }
+
+      @Test
+      @WithUserDetails("viewer", setupBefore=TEST_EXECUTION)
+      fun `cannot get website reports by websiteId, given viewer account`()
+      {
+        // when
+        websiteReportRepository.save(createWebsiteReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          websiteReportService.getWebsiteReportsByWebsite(1)
+        }
+      }
+
+      @Test
+      fun `cannot get website reports by websiteId, given no account`()
+      {
+        // when
+        websiteReportRepository.save(createWebsiteReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          websiteReportService.getWebsiteReportsByWebsite(1)
+        }
+      }
+
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `gets website reports with headers, when no parameters, given admin account`()
+      {
+        // given
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 40L))
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 60L))
+
+        // when
+        val websiteReports = websiteReportService.getWebsiteReports()
+
+        // then
+        websiteReports.page.shouldBe(0)
+        websiteReports.size.shouldBe(2)
+        websiteReports.totalElements.shouldBe(2)
+        websiteReports.totalPages.shouldBe(1)
+        websiteReports.count.shouldBe(2)
+        websiteReports.content.shouldHaveSize(2)
+      }
+
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `gets website reports with headers, when with parameters, given admin account`()
+      {
+        // given
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 40L))
+        websiteReportRepository.save(createWebsiteReportEntity(websiteFk = 60L))
+
+        // when
+        val websiteReports = websiteReportService.getWebsiteReports(DefaultParameters(
+          page = 1,
+          size = 1,
+          sort = "id",
+          order = OrderEnum.ASC
+        ))
+
+        // then
+        websiteReports.page.shouldBe(1)
+        websiteReports.size.shouldBe(1)
+        websiteReports.totalElements.shouldBe(2)
+        websiteReports.totalPages.shouldBe(2)
+        websiteReports.count.shouldBe(2)
+        websiteReports.content.shouldHaveSize(1)
+      }
+    }
+
+    @Nested
+    inner class GetWebsiteReportTests
+    {
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `gets website report, given admin account`()
+      {
+        // when
+        val expected = websiteReportRepository.save(createWebsiteReportEntity()).toModel()
+
+        // then
+        val actual = websiteReportService.getWebsiteReport(expected.id)
+
+        assertEquals(expected, actual)
+      }
+
+      @Test
+      @WithUserDetails("moderator", setupBefore=TEST_EXECUTION)
+      fun `gets website report, given moderator account`()
+      {
+        // when
+        val expected = websiteReportRepository.save(createWebsiteReportEntity()).toModel()
+
+        // then
+        val actual = websiteReportService.getWebsiteReport(expected.id)
+
+        assertEquals(expected, actual)
+      }
+
+      @Test
+      @WithUserDetails("contributor", setupBefore=TEST_EXECUTION)
+      fun `gets website report, given contributor account`()
+      {
+        // when
+        val expected = websiteReportRepository.save(createWebsiteReportEntity()).toModel()
+
+        // then
+        val actual = websiteReportService.getWebsiteReport(expected.id)
+
+        assertEquals(expected, actual)
+      }
+
+      @Test
+      @WithUserDetails("viewer", setupBefore=TEST_EXECUTION)
+      fun `cannot get website report, given viewer account`()
+      {
+        // when
+        val websiteReport = websiteReportRepository.save(createWebsiteReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          websiteReportService.getWebsiteReport(websiteReport.id)
+        }
+      }
+
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `cannot get websiteReport, when websiteReport not exists`()
+      {
+        assertThrows(NoSuchElementException::class.java) {
+          websiteReportService.getWebsiteReport(5000000)
+        }
+      }
+    }
+
+    @Nested
+    inner class DeleteWebsiteReportTests
+    {
+      @Test
+      @WithUserDetails("admin", setupBefore=TEST_EXECUTION)
+      fun `deletes website report, given admin account`()
+      {
+        // when
+        val websiteReport = websiteReportRepository.save(createWebsiteReportEntity()).toModel()
+        val websiteReportsBefore = websiteReportService.getWebsiteReports().content
+
+        // then
+        assertDoesNotThrow {
+          websiteReportService.deleteWebsiteReport(websiteReport.id)
+        }
+
+        assertThrows(NoSuchElementException::class.java) {
+          websiteReportService.getWebsiteReport(websiteReport.id)
+        }
+
+        val websiteReportsAfter = websiteReportService.getWebsiteReports().content
+
+        websiteReportsBefore.shouldContain(websiteReport)
+        websiteReportsAfter.shouldNotContain(websiteReport)
+        websiteReportsBefore.shouldContainAll(websiteReportsAfter)
+        websiteReportsAfter.shouldNotContainAll(websiteReportsBefore)
+      }
+
+      @Test
+      @WithUserDetails("moderator", setupBefore=TEST_EXECUTION)
+      fun `cannot delete website report, given moderator account`()
+      {
+        // when
+        val websiteReport = websiteReportRepository.save(createWebsiteReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          websiteReportService.deleteWebsiteReport(websiteReport.id)
+        }
+      }
+
+      @Test
+      @WithUserDetails("contributor", setupBefore=TEST_EXECUTION)
+      fun `cannot delete website report, given contributor account`()
+      {
+        // when
+        val websiteReport = websiteReportRepository.save(createWebsiteReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          websiteReportService.deleteWebsiteReport(websiteReport.id)
+        }
+      }
+
+      @Test
+      @WithUserDetails("viewer", setupBefore=TEST_EXECUTION)
+      fun `cannot delete website report, given viewer account`()
+      {
+        // when
+        val websiteReport = websiteReportRepository.save(createWebsiteReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          websiteReportService.deleteWebsiteReport(websiteReport.id)
+        }
+      }
+
+      @Test
+      fun `cannot delete website report, given no account`()
+      {
+        // when
+        val websiteReport = websiteReportRepository.save(createWebsiteReportEntity()).toModel()
+
+        // then
+        assertThrows(NoAuthorizationException::class.java) {
+          websiteReportService.deleteWebsiteReport(websiteReport.id)
         }
       }
     }

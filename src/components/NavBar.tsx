12b828b2583825bcd4ebce20c0d6503cd793ee5a
Michael Hofmann <michael.hofmann@ost.ch>
2023-12-28 11:50:31 +0100

    Update navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,20 @@
-import {NavLink} from "react-router-dom"
+import {NavLink, useNavigate} from "react-router-dom"
 import "./NavBar.css"
 import {useTranslation} from "react-i18next";
 import {Button, ButtonGroup} from "@mui/material";
 import {t} from "i18next";
+import {useContext} from "react";
+import {AuthenticationContext} from "../context/AuthenticationContext.ts";
 
 export default function NavBar() {
   const {i18n} = useTranslation()
+  const {authentication} = useContext(AuthenticationContext);
+  const navigate = useNavigate();
 
   const changeLanguage = async (lng: string) => {
     await i18n.changeLanguage(lng);
   }
+
   return (
     <>
       <nav className="navbar">
@@ -20,7 +25,25 @@ export default function NavBar() {
           <li>
             <NavLink to="/websites" end>Websites</NavLink>
           </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/signup">Signup</NavLink>
+          </li>
+          <li>
+            <NavLink to="/profile">Profile</NavLink>
+          </li>
           <li className="languageSelection">
+            <ButtonGroup variant="text" aria-label={t("Navbar.languageSelectionLabel")}>
+              {authentication.isAuthenticated ?
+                <Button sx={{textTransform: 'lowercase'}}
+                        onClick={() => navigate("/logout")}>Logout</Button>
+                :
+                <Button sx={{textTransform: 'lowercase'}}
+                        onClick={() => navigate("/login")}>Login</Button>
+              }
+            </ButtonGroup>
             <ButtonGroup variant="text" aria-label={t("Navbar.languageSelectionLabel")}>
               <Button className="langBtn" disabled={i18n.resolvedLanguage === "de"}
                       onClick={() => changeLanguage("de")}>DE</Button>

14e561e7844d5b73b9532daa9bf2518df1bf3e36
Michael Hofmann <michael.hofmann@ost.ch>
2024-01-02 00:29:16 +0100

    Add oauth logout

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -2,15 +2,20 @@ import {AuthenticationService} from "../services/AuthenticationService.ts";
 import {useContext, useEffect} from "react";
 import {AuthenticationContext} from "../context/AuthenticationContext.ts";
 import {Navigate} from "react-router-dom";
+import {googleLogout} from "@react-oauth/google";
 
 function Logout() {
   const {authentication, setAuthentication} = useContext(AuthenticationContext);
 
   useEffect(() => {
-    if (authentication.isAuthenticated) {
-      AuthenticationService.logout(setAuthentication)
+    if(authentication.isAuthenticated) {
+      if(setAuthentication) {
+        AuthenticationService.logout(setAuthentication)
+      }
     }
   });
+
+  googleLogout();
 
   return <Navigate to="/" />
 }

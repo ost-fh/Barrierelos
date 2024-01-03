efce1628d7ff86d16d6bd4612cbaafaaf513d90f
Pascal Lehmann <pascal.lehmann@ost.ch>
2024-01-03 11:44:39 +0100

    Add OpenAPI title, version and description

--- src/main/kotlin/ch/barrierelos/backend/Application.kt
+++ src/main/kotlin/ch/barrierelos/backend/Application.kt
@@ -1,11 +1,14 @@
 package ch.barrierelos.backend
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @AutoConfiguration
+@OpenAPIDefinition(info = Info(title = "Barrierelos API", version = "1.0.0", description = "API for the Barrierelos project"))
 public class Application
 
 public fun main(args: Array<String>)

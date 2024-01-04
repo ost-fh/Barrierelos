965cff3df3627b421c52818a49a9c218284e851e
Michael Hofmann <michael.hofmann@ost.ch>
2024-01-04 15:39:00 +0100

    Add reporting test data

--- src/main/resources/db/migration/V1.0.1__example_data.sql
+++ src/main/resources/db/migration/V1.0.1__example_data.sql
@@ -8,23 +8,132 @@ TRUNCATE "credential" CASCADE;
 
 INSERT INTO "user"("user_id", "username", "firstname", "lastname", "email", "roles", "deleted")
 VALUES
-  (1, 'admin', 'Hans', 'Root', 'admin@localhost', '{ADMIN}'::role_enum[], FALSE),
-  (2, 'moderator', 'Hans', 'Admin', 'moderator@localhost', '{MODERATOR}'::role_enum[], FALSE),
-  (3, 'contributor', 'Hans', 'Moderator', 'contributor@localhost', '{CONTRIBUTOR}'::role_enum[], FALSE),
-  (4, 'viewer', 'Hans', 'Creator', 'viewer@localhost', '{VIEWER}'::role_enum[], FALSE);
+  (1, 'admin', 'Hans', 'Admin', 'admin@localhost', '{ADMIN}'::role_enum[], FALSE),
+  (2, 'moderator', 'Hans', 'Moderator', 'moderator@localhost', '{MODERATOR}'::role_enum[], FALSE),
+  (3, 'contributor', 'Hans', 'Contributor', 'contributor@localhost', '{CONTRIBUTOR}'::role_enum[], FALSE),
+  (4, 'viewer', 'Hans', 'Viewer', 'viewer@localhost', '{VIEWER}'::role_enum[], FALSE),
+  (10, 'hans.muster', 'Hans', 'Muster', 'hans.muster@barrierelos.ch', '{CONTRIBUTOR}'::role_enum[], FALSE),
+  (11, 'ursula.beispiel', 'Ursula', 'Beispiel', 'ursula.beispiel@barrierelos.ch', '{CONTRIBUTOR}'::role_enum[], FALSE),
+  (12, 'céline.example', 'Céline', 'Example', 'celine.exemple@barrierelos.ch', '{CONTRIBUTOR}'::role_enum[], FALSE),
+  (13, 'giovanni.esempio', 'Giovanni', 'Esempio', 'giovanni.esempio@barrierelos.ch', '{CONTRIBUTOR}'::role_enum[], FALSE),
+  (14, 'bill.gates', 'Bill', 'Gates', 'bill.gates@barrierelos.ch', '{CONTRIBUTOR}'::role_enum[], FALSE),
+  (15, 'elon.musk', 'Elon', 'Musk', 'elon.musk@barrierelos.ch', '{CONTRIBUTOR}'::role_enum[], FALSE),
+  (16, 'twitter', 'Tweeting', 'Twitter', 'twitter@barrierelos.ch', '{CONTRIBUTOR}'::role_enum[], FALSE),
+  (17, 'xxx', 'XXX', 'XXX', 'xxx@barrierelos.ch', '{CONTRIBUTOR}'::role_enum[], FALSE);
 
 SELECT SETVAL('user_user_id_seq', (SELECT MAX("user_id") FROM "user"));
-
 
 INSERT INTO "credential"("credential_id", "user_fk", "password", "issuer", "subject")
 VALUES
   (1, 1, "crypt"('password', "gen_salt"('bf')), NULL, NULL),
   (2, 2, "crypt"('password', "gen_salt"('bf')), NULL, NULL),
   (3, 3, "crypt"('password', "gen_salt"('bf')), NULL, NULL),
-  (4, 4, "crypt"('password', "gen_salt"('bf')), NULL, NULL);
+  (4, 4, "crypt"('password', "gen_salt"('bf')), NULL, NULL),
+  (10, 10, "crypt"('password', "gen_salt"('bf')), NULL, NULL),
+  (11, 11, "crypt"('password', "gen_salt"('bf')), NULL, NULL),
+  (12, 12, "crypt"('password', "gen_salt"('bf')), NULL, NULL),
+  (13, 13, "crypt"('password', "gen_salt"('bf')), NULL, NULL),
+  (14, 14, "crypt"('password', "gen_salt"('bf')), NULL, NULL),
+  (15, 15, "crypt"('password', "gen_salt"('bf')), NULL, NULL),
+  (16, 16, "crypt"('password', "gen_salt"('bf')), NULL, NULL),
+  (17, 17, "crypt"('password', "gen_salt"('bf')), NULL, NULL);
 
 SELECT SETVAL('credential_credential_id_seq', (SELECT MAX("credential_id") FROM "credential"));
 
+
+---------------------------------------------------------------------------
+-- Reports                                                                  --
+---------------------------------------------------------------------------
+
+TRUNCATE "report_message" CASCADE;
+TRUNCATE "webpage_report" CASCADE;
+TRUNCATE "website_report" CASCADE;
+TRUNCATE "user_report" CASCADE;
+TRUNCATE "report" CASCADE;
+TRUNCATE "webpage" CASCADE;
+TRUNCATE "website" CASCADE;
+
+INSERT INTO "website"(  "website_id", "user_fk", "url", "domain", "category", "status", "score")
+VALUES
+  (1, 10, 'https://admin.cha', 'admin.cha', 'GOVERNMENT_FEDERAL'::category_enum, 'READY'::status_enum, 1.0),
+  (2, 10, 'https://www.zuerich.com', 'zuerich.com', 'GOVERNMENT_CANTONAL'::category_enum, 'BLOCKED'::status_enum, 1.0),
+  (3, 10, 'https://zh.ch', 'zh.ch', 'GOVERNMENT_MUNICIPAL'::category_enum, 'READY'::status_enum, 1.0),
+  (4, 10, 'https://www.rapperswil-jona.ch', 'rapperswil-jona.ch', 'GOVERNMENT_MUNICIPAL'::category_enum, 'PENDING_RESCAN'::status_enum, 1.0);
+
+INSERT INTO "webpage"(  "webpage_id", "website_fk", "user_fk", "url", "display_url", "status", "deleted")
+VALUES
+  (1, 1, 10, 'https://admin.cha/troll', '/troll', 'READY'::status_enum, false),
+  (2, 2, 10, 'https://www.zuerich.com/en/visit/shopping/airport-shopping', '/en/visit/shopping/airport-shopping', 'BLOCKED'::status_enum, false),
+  (3, 3, 10, 'https://zh.ch/interkantonales-technikum-rapperswil', '/interkantonales-technikum-rapperswil', 'READY'::status_enum, true),
+  (4, 4, 10, 'https://www.rapperswil-jona.ch/go-jona', '/go-jona', 'PENDING_RESCAN'::status_enum, false);
+
+INSERT INTO "report"("report_id", "user_fk", "reason", "state", "modified", "created")
+VALUES
+  (1, 10, 'MISLEADING'::reason_enum, 'OPEN'::state_enum, '2024-01-01 15:06:33', '2024-01-01 15:06:33'),
+  (2, 10, 'MISLEADING'::reason_enum, 'OPEN'::state_enum, '2024-01-01 17:23:42', '2024-01-01 17:23:44'),
+  (3, 10, 'INCORRECT'::reason_enum, 'CLOSED'::state_enum, '2024-01-01 19:17:11', '2024-01-01 19:17:11'),
+  (4, 10, 'INAPPROPRIATE'::reason_enum, 'OPEN'::state_enum, '2024-01-01 21:34:57', '2024-01-01 21:34:57'),
+
+  (11, 10, 'MISLEADING'::reason_enum, 'OPEN'::state_enum, '2024-01-02 21:34:57', '2024-01-02 21:34:57'),
+  (12, 10, 'INCORRECT'::reason_enum, 'CLOSED'::state_enum, '2024-01-02 21:34:57', '2024-01-02 21:34:57'),
+  (13, 10, 'INCORRECT'::reason_enum, 'OPEN'::state_enum, '2024-01-02 21:34:57', '2024-01-02 21:34:57'),
+  (14, 10, 'INAPPROPRIATE'::reason_enum, 'OPEN'::state_enum, '2024-01-02 21:34:57', '2024-01-02 21:34:57'),
+
+  (21, 10, 'MISLEADING'::reason_enum, 'OPEN'::state_enum, '2024-01-03 21:34:57', '2024-01-03 21:34:57'),
+  (22, 10, 'INCORRECT'::reason_enum, 'OPEN'::state_enum, '2024-01-03 21:34:57', '2024-01-03 21:34:57'),
+  (23, 10, 'INCORRECT'::reason_enum, 'CLOSED'::state_enum, '2024-01-03 21:34:57', '2024-01-03 21:34:57'),
+  (24, 10, 'INAPPROPRIATE'::reason_enum, 'OPEN'::state_enum, '2024-01-03 21:34:57', '2024-01-03 21:34:57');
+
+INSERT INTO "user_report"("user_report_id", "report_fk", "user_fk")
+VALUES
+  (1, 1, 14),
+  (2, 2, 15),
+  (3, 3, 16),
+  (4, 4, 17);
+
+INSERT INTO "website_report"("website_report_id", "report_fk", "website_fk")
+VALUES
+  (1, 11, 1),
+  (2, 12, 2),
+  (3, 13, 3),
+  (4, 14, 4);
+
+INSERT INTO "webpage_report"("webpage_report_id", "report_fk", "webpage_fk")
+VALUES
+  (1, 21, 1),
+  (2, 22, 2),
+  (3, 23, 3),
+  (4, 24, 4);
+
+INSERT INTO "report_message"("report_message_id", "report_fk", "user_fk", "message", "modified", "created")
+VALUES
+  (1, 1, 10, 'This is not the real Bill Gates, I think.', '2024-01-01 15:06:33', '2024-01-01 15:06:33'),
+  (2, 2, 10, 'Someone is impersonating Elon Musk here.', '2024-01-01 17:23:44', '2024-01-01 17:23:44'),
+  (3, 3, 11, 'Twitter is no longer correct, it is now called X.', '2024-01-01 19:17:11', '2024-01-01 19:17:11'),
+  (4, 4, 10, 'This username is really inappropriate.', '2024-01-01 21:34:57', '2024-01-01 21:34:57'),
+  (5, 3, 10, 'I refuse to call it X, it is still Twitter to me.', '2024-01-01 23:14:28', '2024-01-01 23:14:28'),
+  (6, 3, 13, 'I agree, X makes no sense.', '2024-01-02 04:03:12', '2024-01-02 04:03:12'),
+  (7, 3, 2, 'It is officially called X now, so case closed.', '2024-01-02 05:17:49', '2024-01-02 05:17:49'),
+
+  (11, 11, 10, 'This website is definitely misleading. The top-level domain is wrong.', '2024-01-02 15:06:33', '2024-01-02 15:06:33'),
+  (12, 12, 10, 'This is not a government website.', '2024-01-02 17:23:44', '2024-01-02 17:23:44'),
+  (13, 13, 11, 'This is not the website of the city of Zürich, but the Kanton Zürich.', '2024-01-02 19:17:11', '2024-01-02 19:17:11'),
+  (14, 14, 10, 'Jona should not be a part of Rapperswil. Free Jona!', '2024-01-02 21:34:57', '2024-01-02 21:34:57'),
+
+  (21, 21, 10, 'This webpage is just there for trolling.', '2024-01-03 15:06:33', '2024-01-03 15:06:33'),
+  (22, 22, 10, 'It should be airport-shopping, not airport-einkaufen.', '2024-01-03 17:23:44', '2024-01-03 17:23:44'),
+  (23, 23, 11, 'The ITR Interkantonales Technikum Rapperswil no longer exists, it is now called HSR Hochschule für Technik Rapperswil.', '2024-01-03 19:17:11', '2024-01-03 19:17:11'),
+  (24, 24, 10, 'Jona is the real deal. Go Jona!', '2024-01-03 21:34:57', '2024-01-03 21:34:57'),
+  (25, 23, 10, 'No, it is not called HSR Hochschule für Technik Rapperswil anymore either, its current name is OST – Ostschweizer Fachhochschule.', '2024-01-03 23:14:28', '2024-01-03 23:14:28'),
+  (26, 24, 2, 'Yes, OST it is now. Report closed.', '2024-01-04 05:17:49', '2024-01-04 05:17:49');
+
+SELECT SETVAL('"website_website_id_seq"', (SELECT MAX("website_id") FROM "website"));
+SELECT SETVAL('"webpage_webpage_id_seq"', (SELECT MAX("webpage_id") FROM "webpage"));
+SELECT SETVAL('"report_report_id_seq"', (SELECT MAX("report_id") FROM "report"));
+SELECT SETVAL('"user_report_user_report_id_seq"', (SELECT MAX("user_report_id") FROM "user_report"));
+SELECT SETVAL('"website_report_website_report_id_seq"', (SELECT MAX("website_report_id") FROM "website_report"));
+SELECT SETVAL('"webpage_report_webpage_report_id_seq"', (SELECT MAX("webpage_report_id") FROM "webpage_report"));
+SELECT SETVAL('"report_message_report_message_id_seq"', (SELECT MAX("report_message_id") FROM "report_message"));
 
 ---------------------------------------------------------------------------
 -- Tags                                                                  --

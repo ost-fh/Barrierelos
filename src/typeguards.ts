fb4da90106ada373448c435e6feb0a963271ee0c
Michael Hofmann <michael.hofmann@ost.ch>
2024-01-09 16:22:17 +0100

    Add type guards

--- src/typeguards.ts
+++ src/typeguards.ts
@@ -1,4 +1,4 @@
-import {UserReport, WebpageReport, WebsiteReport} from "./lib/api-client";
+import {User, UserReport, Webpage, WebpageReport, Website, WebsiteReport} from "./lib/api-client";
 
 export function isWebsiteReport(report: WebsiteReport | WebpageReport | UserReport): report is WebsiteReport {
   return ("websiteId" in report);
@@ -10,4 +10,16 @@ export function isWebpageReport(report: WebsiteReport | WebpageReport | UserRepo
 
 export function isUserReport(report: WebsiteReport | WebpageReport | UserReport): report is UserReport {
   return ("userId" in report);
+}
+
+export function isWebsite(subject: Website | Webpage | User): subject is Website {
+  return ("domain" in subject);
+}
+
+export function isWebpage(subject: Website | Webpage | User): subject is Webpage {
+  return ("displayUrl" in subject);
+}
+
+export function isUser(subject: Website | Webpage | User): subject is User {
+  return ("username" in subject);
 }

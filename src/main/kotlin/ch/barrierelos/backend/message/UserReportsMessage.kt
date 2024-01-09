dd215530a72b08fdc67750435abf363cb5facd24
Michael Hofmann <michael.hofmann@ost.ch>
2024-01-09 17:58:33 +0100

    Add encode default to reports messages

--- src/main/kotlin/ch/barrierelos/backend/message/UserReportsMessage.kt
+++ src/main/kotlin/ch/barrierelos/backend/message/UserReportsMessage.kt
@@ -3,12 +3,18 @@ package ch.barrierelos.backend.message
 import ch.barrierelos.backend.model.ReportMessage
 import ch.barrierelos.backend.model.User
 import ch.barrierelos.backend.model.UserReport
+import kotlinx.serialization.EncodeDefault
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 
 @Serializable
+@OptIn(ExperimentalSerializationApi::class)
 public data class UserReportsMessage
 (
+  @EncodeDefault
   public val reports: Set<UserReport> = setOf(),
+  @EncodeDefault
   public val messages: Set<ReportMessage> = setOf(),
+  @EncodeDefault
   public val users: Set<User> = setOf(),
 )

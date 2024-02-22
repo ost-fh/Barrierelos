bf2a957c7c05aeff2f7e445f1eb555d9bedafd42
Simon <simon.hager@ost.ch>
2024-02-22 13:08:57 +0100

    change google client id

--- frontend/src/util/constants.ts
+++ frontend/src/util/constants.ts
@@ -1,7 +1,7 @@
 export const ERROR_UNAUTHORIZED = 401;
 export const ERROR_CONFLICT = 409;
 
-export const OAUTH_GOOGLE_CLIENT_ID = "853032647850-896jhdp7s4ck8e1pqlmoktibs8a2ih2i.apps.googleusercontent.com";
+export const OAUTH_GOOGLE_CLIENT_ID = "791499076585-bn1sfeg0r4ip1gb0b6uctmtir8gn6pjp.apps.googleusercontent.com";
 
 export const MIN_LENGTH_USERNAME = 3
 export const MIN_LENGTH_PASSWORD = 6
